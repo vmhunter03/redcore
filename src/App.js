e1d366b4961a69cd097eef7f5af9d4af54ff909a
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-07-06 19:54:43 +0800

    done with Filereader Constractor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-
+import "./App.css";
+import logo from "./assets/image001.png";
 function App() {
+  const showFile = () => {
+    const preview = document.getElementById("show-text");
+    const file = document.querySelector("input[type=file]").files[0];
+    const reader = new FileReader();
+
+    const textFile = /text.*/;
+
+    if (file.type.match(textFile)) {
+      reader.onload = function (e) {
+        preview.innerHTML = e.target.result;
+      };
+    }
+    reader.readAsText(file);
+  };
+
   return (
     <div className="App">
-      
-      <h1>Batman</h1>
+      <img src={logo} alt="logo" />
+      <h1>INITIAL STAGE for React</h1>
+      <span>Select a Book File : </span>
+      <div>
+        <input type="file" onChange={showFile} />
+        <div id="show-text">Choose text File</div>
+      </div>
     </div>
   );
 }
